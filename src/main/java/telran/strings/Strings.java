4c3320132e4af35f21a8ca5d3fda8e326cbc0bc9
Stan Mayer <sthumor@gmail.com>
2024-07-31 22:03:19 +0300

    hw_strings_validating_arithmetics

--- src/main/java/telran/strings/Strings.java
+++ src/main/java/telran/strings/Strings.java
@@ -1,7 +1,22 @@
 package telran.strings;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Strings {
+    static final String keyWords[] = { "abstract", "assert", "boolean",
+            "break", "byte", "case", "catch", "char", "class", "const",
+            "continue", "default", "do", "double", "else", "enum", "extends", "false",
+            "final", "finally", "float", "for", "goto", "if", "implements",
+            "import", "instanceof", "int", "interface", "long", "native",
+            "new", "null", "package", "private", "protected", "public",
+            "return", "short", "static", "strictfp", "super", "switch",
+            "synchronized", "this", "throw", "throws", "transient", "true",
+            "try", "void", "volatile", "while" };
+
     public static String firstName() {
         // regex for strings startin with
         // capital letter and rest as a
@@ -14,6 +29,92 @@ public class Strings {
         // TODO
         // regular expression for testing synthax
         // of Java variable names
-        return "^(_{2,}|_?(\\$|[A-Za-z]))((\\$|\\w)+)?";
+        return "[A-Za-z$]|[A-Z-a-z$_][\\w\\d$]+";
+    }
+
+    public static String number0_300() {
+        return "[1-9]\\d?|[1-2]\\d\\d|300|0";
+    }
+
+    public static String ipV4octet() {
+        return "([0-1]?\\d{1,2}|2([0-4]\\d|5[0-5]))";
+    }
+
+    public static String ipV4Address() {
+        String octet = ipV4octet();
+        return String.format("%s(\\.%s){3}", octet, octet);
+    }
+
+    public static String stringWithJavaNames(String names) {
+        String[] tokens = names.split("\\s+");
+        int i = getJavaNameIndex(tokens, -1);
+        String res = "";
+        if (i >= 0) {
+            res = tokens[i];
+        }
+        while ((i = getJavaNameIndex(tokens, i)) > 0) {
+            res += " " + tokens[i];
+        }
+        ;
+        return res;
+    }
+
+    private static int getJavaNameIndex(String[] tokens, int i) {
+        i++;
+        while (i < tokens.length && !isJavaName(tokens[i])) {
+            i++;
+        }
+        return i < tokens.length ? i : -1;
+    }
+
+    private static boolean isJavaName(String string) {
+        return string.matches(javaVariable()) && Arrays.binarySearch(keyWords, string) < 0;
+    }
+
+    public static boolean isArithmeticExpression(String someExpression) {
+        someExpression = lowExprDepth(someExpression);
+        boolean arithmeticFlag = false;
+        while (someExpression != null && !someExpression.matches("^\\(someExpr$|^someExpr\\)$")
+                && !arithmeticFlag && !someExpression.isEmpty()) {
+            if (someExpression.matches("^someExpr$")) {
+                arithmeticFlag = true;
+            } else {
+                someExpression = lowExprDepth(someExpression);
+            }
+        }
+        return arithmeticFlag;
+    }
+
+    public static String lowExprDepth(String expression) {
+        String group1 = "[^\\(\\)\\+\\-\\/\\*]+";
+        String group2 = "[\\+\\-\\/\\*]";
+        String regex = String.format("(\\(%1$s(%2$s%1$s)+\\))|(%1$s(%2$s%1$s)+)|(\\(%1$s\\))(?!$)|(?!^)(\\(%1$s\\))",
+                group1, group2);
+        expression = expression.replaceAll("\\s+", "");
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(expression);
+        String expressionOut = "";
+        while (matcher.find() && expression != null) {
+            if (isFoundGroup(matcher.group())) {
+                expressionOut = expression.replace(matcher.group(), "someExpr");
+            } else {
+                expressionOut = null;
+            }
+        }
+        return expressionOut;
+    }
+
+    public static Boolean isFoundGroup(String string) {
+        string = string.replaceAll("\\(|\\)", "");
+        String[] groupElements = string.split("\\+|\\-|\\/|\\*");
+        boolean elementFlag = true;
+        int i = 0;
+        while (i < groupElements.length && elementFlag) {
+            if (!groupElements[i].matches("^\\d+$")) {
+                elementFlag = isJavaName(groupElements[i]);
+            }
+            i++;
+        }
+        return elementFlag;
     }
 }
--- src/test/java/telran/strings/RegularExpressionsTests.java
+++ src/test/java/telran/strings/RegularExpressionsTests.java
@@ -1,8 +1,11 @@
 package telran.strings;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static telran.strings.Strings.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -38,5 +41,105 @@ public class RegularExpressionsTests {
         assertFalse("".matches(javaVariable()));
         assertFalse("__||".matches(javaVariable()));
         assertFalse("_".matches(javaVariable()));
+    }
+    @Test
+    void number0_300TestTrue() {      
+        String regex = Strings.number0_300();
+        assertTrue("0".matches(regex));
+        assertTrue("300".matches(regex));
+        assertTrue("250".matches(regex));
+        assertTrue("25".matches(regex));
+        assertTrue("12".matches(regex));
+        assertTrue("299".matches(regex));
+        assertTrue("199".matches(regex));
+        assertTrue("111".matches(regex));
+    }
+    void number0_300TestFalse() {      
+        String regex = Strings.number0_300();
+        assertFalse("00".matches(regex));
+        assertFalse("301".matches(regex));
+        assertFalse("01".matches(regex));
+        assertFalse("1(".matches(regex));
+        assertFalse("100".matches(regex));
+        assertFalse("20".matches(regex));
+        assertFalse("100".matches(regex));
+        assertFalse("410".matches(regex));
+
+    }
+    @Test
+    void ipV4octetTrueTest() {
+        String regex = Strings.ipV4octet();
+        assertTrue("0".matches(regex));
+        assertTrue("000".matches(regex));
+        assertTrue("10".matches(regex));
+        assertTrue("100".matches(regex));
+        assertTrue("255".matches(regex));
+        assertTrue("199".matches(regex));
+        assertTrue("001".matches(regex));
+
+    }
+    @Test
+    void ipV4octetFalseTest() {
+        String regex = Strings.ipV4octet();
+        assertFalse("256".matches(regex));
+        assertFalse("1000".matches(regex));
+        assertFalse("12bla".matches(regex));
+        assertFalse(".234".matches(regex));
+        assertFalse("255.".matches(regex));
+        assertFalse("-1".matches(regex));
+        assertFalse("0001".matches(regex));
+        assertFalse(" ".matches(regex));
+    }
+    @Test
+    void ipV4AddressTrueTest() {
+        String regex = Strings.ipV4Address();
+        assertTrue("0.0.0.0".matches(regex));
+        assertTrue("100.100.100.100".matches(regex));
+        assertTrue("255.255.255.255".matches(regex));
+    }
+    @Test
+    void ipV4AddressFalseTest() {
+        String regex = Strings.ipV4Address();
+        assertFalse("0.0.0,0".matches(regex));
+        assertFalse("1.1.1".matches(regex));
+        assertFalse("23.23.23. ".matches(regex));
+        assertFalse("1234.50.18.18".matches(regex));
+        assertFalse("0".matches(regex));
+        assertFalse("0._.0.0".matches(regex));
+        assertFalse("0._.0 0".matches(regex));
+    }
+    @Test
+    void stringWithJavaNamesTest() {
+        String names = "123  1a   abs int     enum null lmn";
+        String expected = "abs lmn";
+        assertEquals(expected, Strings.stringWithJavaNames(names));
+    }
+    @Test
+    void isArithmeticExpressionTest() {
+        assertTrue(isArithmeticExpression("12+111"));
+        assertTrue(isArithmeticExpression("12   - 10"));
+        assertTrue(isArithmeticExpression("(64) /     11"));
+        assertTrue(isArithmeticExpression("(45) *(18)"));
+        assertTrue(isArithmeticExpression("10 - expression"));
+        assertTrue(isArithmeticExpression("expression  +  (other)"));
+        assertTrue(isArithmeticExpression("(34 - (expression))"));
+        assertTrue(isArithmeticExpression("(int1 - int2/(int3))*89"));
+        assertTrue(isArithmeticExpression("__56$ + 16      / (11) * $18 * float12$3"));
+        assertTrue(isArithmeticExpression("(6) - 8 + int56"));
+        assertTrue(isArithmeticExpression("3 / unb17 + (_3)"));
+        assertTrue(isArithmeticExpression("(((1150+5)/(3*(inn-(aaaa)  +45/345)))    *(a434 -a643434* __ -  45))"));
+
+        assertFalse(isArithmeticExpression(""));
+        assertFalse(isArithmeticExpression("(_4 - /+expression)"));
+        assertFalse(isArithmeticExpression("(4)"));
+        assertFalse(isArithmeticExpression("4"));
+        assertFalse(isArithmeticExpression("assembly"));
+        assertFalse(isArithmeticExpression("asdfdf + 1_"));
+        assertFalse(isArithmeticExpression("\"(((1150+5)/(3*(inn-(25aa)  +45/345)))    *(a434 -a64mnmn4* __ -  4_5))\""));
+        assertFalse(isArithmeticExpression("(((1150+5)/(3*(.....-(aaaa)  +45/345)))    *(a434 -a643434* __ -  45))"));
+        assertFalse(isArithmeticExpression("sdsint"));
+        assertFalse(isArithmeticExpression("(6-uri))"));
+        assertFalse(isArithmeticExpression(":';;s___"));
+        
     }
 }
